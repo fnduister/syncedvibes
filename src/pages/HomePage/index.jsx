6b79ef7a532922a56126c3ddcaf5eba5aee5ad9a
Fabrice Ndui <fnduister@gmail.com>
2018-12-21 22:32:20 -0500

    working on the main content

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+const Container = styled(Grid)`
+  flex-grow: 1;
+`;
 
 const HomePage = () => {
   return (
-    <div>
-      <p>homepage</p>
-    </div>
+    <Container
+      container
+      spacing={16}
+      alignItems="center"
+      direction="column"
+      justify="center"
+    >
+      <Grid item sm={8}>
+        <Typography variant="h4" color="textPrimary">
+          Iamsu - FreeStyles
+        </Typography>
+        <Typography variant="body" color="textPrimary">
+          9 hours ago by markvok
+        </Typography>
+        <Typography variant="body" color="textPrimary">
+          Video
+        </Typography>
+        <Typography variant="body" color="textPrimary">
+          Text
+        </Typography>
+      </Grid>
+    </Container>
   );
 };
 
