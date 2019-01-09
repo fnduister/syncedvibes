48b00f9f6fe197471f15b21b72508f42b300f09e
Fabrice Ndui <fnduister@gmail.com>
2019-01-08 23:08:43 -0500

    fixed comment section

--- src/containers/CommentSec/CommentSec.jsx
+++ src/containers/CommentSec/CommentSec.jsx
@@ -1,35 +1,38 @@
 import React, { Fragment, Component } from "react";
-import {CommentBox, CommentList, CommentForm, Comment} from '../../components/CommentSection/CommentSection';
 import {
-  Comment 
-} from "./styled.js";
+  CommentBox,
+  CommentList,
+  CommentForm,
+  Comment
+} from "../../components/CommentSection/CommentSection";
 
-class CommentSec extends Component{
+class CommentSec extends Component {
   state = {
     commentData: [
-      { 
-        author: "User1", 
+      {
+        author: "User1",
         text: "Sample Comment 1"
       },
-      { 
-        author:"User2", 
-        text:"Sample Comment 2" 
+      {
+        author: "User2",
+        text: "Sample Comment 2"
       }
     ]
-  }
+  };
 
-
-  render(){
+  render() {
     return (
       <Fragment color="default" variant="body1">
-      <CommentBox />,
-      <CommentForm/>,
-      <CommentList/>,
-      <Comment  author={this.state.commentData[0].author} text={this.state.commentData[0].text}/>
+        <CommentBox />,
+        <CommentForm />,
+        <CommentList />,
+        <Comment
+          author={this.state.commentData[0].author}
+          text={this.state.commentData[0].text}
+        />
       </Fragment>
-    )
+    );
   }
-};
-
+}
 
 export default CommentSec;
