58515b09f6cbca31571491fad88f783a19547f6f
Fabrice Ndui <fnduister@gmail.com>
2019-05-01 21:41:04 -0400

    changed color for notifications

--- src/components/Notification/styled.js
+++ src/components/Notification/styled.js
@@ -4,32 +4,34 @@ import SnackbarContent from "@material-ui/core/SnackbarContent";
 import Typography from "@material-ui/core/Typography";
 
 const variantTheme = variant => {
-    switch (variant) {
-        case "success":
-            return "#6BC13A";
-        case "warning":
-            return "#DD4944";
-        case "error":
-            return "red";
-        default:
-        return "green";
-    }
-}
+  switch (variant) {
+    case "success":
+      return "#6BC13A";
+    case "error":
+      return "#DD4944";
+    case "warning":
+      return "#ef833b";
+    case "info":
+      return "#8eb7f9";
+    default:
+      return "green";
+  }
+};
 
 export const SnackbarStyled = styled(Snackbar)`
     /* background-color: ${props => variantTheme(props.variant)}; */
 `;
 
 export const SnackbarContentStyled = styled(SnackbarContent)`
-    background-color: ${props => variantTheme(props.variant)};
+  background-color: ${props => variantTheme(props.variant)};
 `;
 
 export const MessageBox = styled.span`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Message = styled(Typography)`
-    margin-left: .4em;
+  margin-left: 0.4em;
 `;
