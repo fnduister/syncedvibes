cb5333559f7375498b387465cc0728d174288f92
fabrice <fnduister@gmail.com>
2019-10-16 00:05:57 -0400

    in case that was good

--- src/components/AddArticle/AddArticle.jsx
+++ src/components/AddArticle/AddArticle.jsx
@@ -1,30 +1,33 @@
-import React, { Fragment, useState } from "react";
-import { Formik } from "formik";
-import { compose } from "redux";
-import { withHandlers } from "recompose";
-import { withRouter } from "react-router";
-import { firebaseConnect, isLoaded } from "react-redux-firebase";
-import { DialogContentStyled, DialogTitleStyled } from "./styled";
-import EditFormSchema from "../Forms/EditForm/EditFormSchema";
-import moment from "moment";
-import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
-import { connect } from "react-redux";
-import "moment-timezone";
-import EditForm from "../Forms/EditForm/EditForm";
-import { openNotification } from "../Notification/reducer";
-import Axios from "axios";
+import React, { Fragment, useState } from 'react';
+import { Formik } from 'formik';
+import { compose } from 'redux';
+import { withHandlers } from 'recompose';
+import { withRouter } from 'react-router';
+import { firebaseConnect, isLoaded } from 'react-redux-firebase';
+import { DialogContentStyled, DialogTitleStyled } from './styled';
+import EditFormSchema from '../Forms/EditForm/EditFormSchema';
+import moment from 'moment';
+import { EditorState, convertToRaw, convertFromRaw } from 'draft-js';
+import { connect } from 'react-redux';
+import 'moment-timezone';
+import EditForm from '../Forms/EditForm/EditForm';
+import { openNotification } from '../Notification/reducer';
+import Axios from 'axios';
 import SearchBar from '../SearchBar/SearchBar';
-import GifList from '../GifList/GifList'
+import GifList from '../GifList/GifList';
 import request from 'superagent';
 
-let gifs = []
-const handleTermChange = term => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
+let gifs = [];
+const handleTermChange = (term) => {
+  const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(
+    /\s/g,
+    '+',
+  )}&api_key=dc6zaTOxFJmzC`;
 
   request.get(url, (err, res) => {
-       gifs= res.body.data 
+    gifs = res.body.data;
   });
-}
+};
 const AddArticle = ({
   settings,
   article,
@@ -37,57 +40,51 @@ const AddArticle = ({
   location,
   history,
 }) => {
-
   let contentState = {};
   let articlesDefault = {};
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState('');
   if (article) {
-    contentState = article.hasOwnProperty("content")
+    contentState = article.hasOwnProperty('content')
       ? convertFromRaw(JSON.parse(article.content))
       : null;
     articlesDefault = { ...article };
   }
   if (!isLoaded(settings)) return <div>...loading</div>;
   return (
-
     <Fragment>
       <DialogContentStyled>
-        <DialogTitleStyled color="secondary">Add Article</DialogTitleStyled>
+        <DialogTitleStyled color='secondary'>Add Article</DialogTitleStyled>
         <Formik
           initialValues={{
             ...articlesDefault,
             editorState: !add
               ? new EditorState.createWithContent(contentState)
-              : new EditorState.createEmpty()
+              : new EditorState.createEmpty(),
           }}
-
           validationSchema={EditFormSchema}
-     
           onSubmit={async (values, actions) => {
             actions.setSubmitting(false);
-            const content = JSON.stringify(
-              convertToRaw(values.editorState.getCurrentContent())
-            );
+            const content = JSON.stringify(convertToRaw(values.editorState.getCurrentContent()));
             const { title, type, thumbnail, media } = values;
             const date = moment().format();
             const formData = new FormData();
-            formData.append("file", file);
+            formData.append('file', file);
             const dataToSave = {
               title,
               type,
               media,
               thumbnail,
               date,
-              content
+              content,
             };
 
             try {
               if (add) {
-                console.log("tryinhg");
-                await Axios.post("http://localhost:5000/upload", formData, {
+                console.log('tryinhg');
+                await Axios.post('http://localhost:5000/upload', formData, {
                   headers: {
-                    "Content-Type": "multipart/form-data"
-                  }
+                    'Content-Type': 'multipart/form-data',
+                  },
                 });
               }
             } catch (err) {
@@ -97,50 +94,45 @@ const AddArticle = ({
             if (edit) {
               editHandler();
               updateArticle(dataToSave);
-              openNotificationHandler("Modification Saved", "success");
+              openNotificationHandler('Modification Saved', 'success');
             }
 
             if (add) {
               saveArticle(dataToSave);
-              openNotificationHandler("Article Added", "success");
-              history.push("/");
+              openNotificationHandler('Article Added', 'success');
+              history.push('/');
             }
 
             actions.setSubmitting(false);
-            actions.setStatus({ msg: "Recorded" });
+            actions.setStatus({ msg: 'Recorded' });
           }}
-          render={formikProps => (
-            <EditForm
-              types={settings.types}
-              {...formikProps}
-              setFile={setFile}
-            />
+          render={(formikProps) => (
+            <EditForm types={settings.types} {...formikProps} setFile={setFile} />
           )}
         />
-        {console.log("dans addArticle")}
+        {console.log('dans addArticle')}
       </DialogContentStyled>
       <div>
-      <SearchBar  onTermChange={handleTermChange}  />
-      <GifList gifs={gifs} />
+        <SearchBar onTermChange={handleTermChange} />
+        <GifList gifs={gifs} />
       </div>
     </Fragment>
   );
 };
 
 const enhance = compose(
-  firebaseConnect(() => ["articles", "settings"]),
+  firebaseConnect(() => ['articles', 'settings']),
   connect(
     ({ firebase }, props) => ({
-      settings: firebase.data.settings // lodash's get can also be used
+      settings: firebase.data.settings, // lodash's get can also be used
     }),
     {
-      openNotificationHandler: (message, variant) =>
-        openNotification(message, variant)
-    }
+      openNotificationHandler: (message, variant) => openNotification(message, variant),
+    },
   ),
   withRouter,
   withHandlers({
-    saveArticle: props => data => props.firebase.push("articles", data)
-  })
+    saveArticle: (props) => (data) => props.firebase.push('articles', data),
+  }),
 );
 export default enhance(AddArticle);
